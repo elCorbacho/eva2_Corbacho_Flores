2dfb2dd2c60ab38d8b728b9616fc8bb2202c3ffc
ElCorbacho <histopat.cor@gmail.com>
2025-08-15 22:56:44 -0400

    se agrega bateria de pruebas en postman

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,7 +29,7 @@ class AuthController extends Controller
         return response()->json(['user' => $user, 'token' => $token], 201);
     }
 
-     // Funcion para registrar usuario desde la web y mostrar modal
+     // Funcion para registrar usuario desde la web
     public function registerWeb(Request $request)
     {
         $request->validate([
@@ -44,7 +44,7 @@ class AuthController extends Controller
             'password' => bcrypt($request->password)
         ]);
 
-        // Redirige con variable de sesión para mostrar el modal
+        //
         return redirect()->back()->with('usuario_creado', true);
     }
 
